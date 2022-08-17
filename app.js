65fcbad15440ab81090e574680dc32b685f52e91
Tugsuu <tugsking2002@gmail.com>
2022-08-17 12:24:02 +0800

    Shoog shiddeg bolgox

--- app.js
+++ app.js
@@ -8,17 +8,24 @@ var scores = [0 , 0 ];
 var roundScores = 0;
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч , 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэнэ 
-var dice = Math.floor(Math.random()*6)+1; 
+
 
 
 
 
 // Программ эхлэхэд бэлтгэнэ
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
-document.querySelector('.dice').style.display = 'none';
 
-console.log("Шоо : " + dice);
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+var diceDom = document.querySelector('.dice');
+diceDom.style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click' , function(){
+    var diceNumber = Math.floor(Math.random()*6)+1; 
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + diceNumber + '.png';
+});
